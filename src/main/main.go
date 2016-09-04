ab97293b6c0f384807b7f1f35b0c35ddecac329e
Leonli <leonlee0811@gmail.com>
2016-09-04 20:55:08 +0800

    added talib

--- src/main/main.go
+++ src/main/main.go
@@ -5,8 +5,11 @@ import(
 	//"net/http"
 	"debugger"
 	//"os"
-	"razor"
-	"scheduler"
+	//"razor"
+	//"scheduler"
+	//"math"
+	//"github.com/d4l3k/talib"
+
 )
 
 func main() {
@@ -25,21 +28,22 @@ func main() {
 
 
 	//razor.GetLiveStockData_GoogleAPI()
-	razor.InitiateTickermap()
+	//razor.InitiateTickermap()
          //razor.Rz()
 //----------------------------------------------------------------------
-	scheduler.NewSchedule("GetLiveStockFromGoogle")
+	//scheduler.NewSchedule("GetLiveStockFromGoogle")
 	//scheduler.NewJob("get stock every 1min","0 0/1 * ? 1-12 1-5",razor.GetLiveStockData_GoogleAPI)
 
 	//test---------------------------------------------------------------------
 	//scheduler.NewJob("1s","@every 1s",func() { fmt.Println("Every 1s") })
-        scheduler.NewJob("5s","@every 5s",razor.GetLiveStockData_GoogleAPI)
+        //scheduler.NewJob("5s","@every 5s",razor.GetLiveStockData_GoogleAPI)
 	// test------------------------------------------------------------------
 
-	scheduler.ScheduleStart()
+	//scheduler.ScheduleStart()
 
 
 
+	//fmt.Println(talib.Sin([]float64{0, math.Pi / 2}))
 
 }
 
